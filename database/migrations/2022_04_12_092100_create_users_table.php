4c8d6ffe168fd93ec717509254a3887aa704554d
dangtrungduc1993 <trungduc.hou@gmail.com>
2022-04-13 10:07:28 +0700

    add userseeder

--- database/migrations/2022_04_12_092100_create_users_table.php
+++ database/migrations/2022_04_12_092100_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('address')->nullable();
             $table->integer('phone')->nullable()->unique();
             $table->rememberToken();
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,24 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $category = new Category();
+        $category->name = "Nhac Tre";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Nhac Vang";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Nhac Rap";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Nhac Tru Tinh";
+        $category->save();
+
+        $category = new Category();
+        $category->name = "Nhac Do";
+        $category->save();
     }
 }
